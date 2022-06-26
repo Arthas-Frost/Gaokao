38b52529ab260f7ddc40d850d4461d583602060d
Arthas-Frost <72306936+Arthas-Frost@users.noreply.github.com>
2022-06-26 20:54:58 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+[Documentation]:https://img.shields.io/badge/-示例(可点)-blue.svg?logo=Wikipedia&style=for-the-badge&logoColor=black
+
+# 高考倒计时
+
+[![Documentation]](https://tartaros.fun/GaoKao)
+- 添加了背景的斑点
+- 添加了高三倒计时
+- 适配了手机
+- 黑夜模式为默认
+- 添加了一个[音乐](https://music.163.com/song?id=1385176005)
+
+## 实现参考
+
+进度条采用: [vue-simple-progress](https://github.com/dzwillia/vue-simple-progress)
+
+Digital Clock 和大体的 CSS  是从 [Digital Clock with Vue.js - Toshiyuki TAKAHASH](https://codepen.io/gau/pen/LjQwGp) 处剽窃过来的
+
+高考倒计时的实现参考了 [MicDZ的文章](https://www.micdz.cn/article/time/)
+
+## 演示案例
+- 更改
+![](./light_modified.png)
+![](./night_modified.png)
+
+
+- 原来长这样
+![](./Mixed.png)
+
+![](./Night.png)
+
+![](./Light.png)
---
+++ script.js
@@ -0,0 +1,75 @@
+var clock = new Vue({
+    el: '#clock',
+    data: {
+        time: '',
+        date: '',
+        cur: '',
+        tcur: '',
+        lst_d: '',
+        lst_h: '',
+        lst_m: '',
+        lst_s: ''
+    }
+});
+
+var week = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+function updateTime() {
+    var cd = new Date();
+    clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    var EndTime = new Date("2022/06/07 09:00:00"); //截止时间
+    var t = (EndTime.getTime() - cd.getTime()) / 1000;
+    clock.lst_d = zeroPadding(Math.floor(t / 86400), 3);
+    clock.lst_h = zeroPadding(Math.floor((t /3600) % 24), 2);
+    clock.lst_m = zeroPadding(Math.floor((t / 60) % 60), 2);
+    clock.lst_s = zeroPadding(Math.floor(t % 60), 2);
+};
+
+function zeroPadding(num, digit) {
+    var zero = '';
+    for(var i = 0; i < digit; i++) {
+        zero += '0';
+    }
+    return (zero + num).slice(-digit);
+};
+
+var todayleast = setInterval(todaylst, 8640);
+todaylst();
+function todaylst() {
+    var NowTime = new Date();
+    clock.cur = Math.floor(((NowTime.getTime() / 1000) + 28800) % 86400) / 864;
+    clock.cur = clock.cur.toFixed(2); 
+};
+
+var termleast = setInterval(termlst, 8640);
+termlst();
+function termlst() {
+    var starTime = new Date("2021/06/07 09:00:00").getTime();
+    var termTime = new Date("2022/06/07 09:00:00").getTime();
+    var NowTime = new Date().getTime();
+    clock.tcur = Math.floor((NowTime-starTime) / (termTime-starTime) * 100);
+    clock.tcur = clock.tcur.toFixed(2); 
+};
+
+function change(a){
+    var css=document.getElementById("css");
+    if (a == 1) {
+        css.setAttribute("href","night.css");
+        bubbly({
+          colorStart: "#181520",
+          colorStop: "#181520",
+          shadowColor: "#fff7fe",
+          bubbleFunc: () => `hsla(${Math.random() * 360}, 0%, 100%, ${Math.random() * 0.3})`
+        });
+    }
+    if (a == 2) {
+        css.setAttribute("href","light.css");
+        bubbly({
+          colorStart: "#a3b8c8",
+          colorStop: "#05313d",
+          bubbleFunc: () => `hsla(0, 100%, 50%, ${Math.random() * 0.25})`
+        });
+    }
+}
